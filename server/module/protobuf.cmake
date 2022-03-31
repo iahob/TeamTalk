cb5661d0ae8892c3e070fb75889902e697c85945
yuhaibo <yu.haibo@yalla.live>
2022-03-31 13:47:49 +0800

    配置文件更新，lib 文件的更新

--- server/module/protobuf.cmake
+++ server/module/protobuf.cmake
@@ -1,10 +1,10 @@
 include(ExternalProject)
 
-set(PROTOBUF_NAME protobuf-all-3.19.4)
+set(PROTOBUF_NAME protobuf-all-3.18.1)
 set(PROTOBUF_ROOT            ${CMAKE_CURRENT_BINARY_DIR}/${PROTOBUF_NAME})
 set(HIREDIS_SOURCE_PATH      ${PROTOBUF_ROOT}/src/${PROTOBUF_NAME})
 
-set(PROTOBUF_URL             ${CMAKE_SOURCE_DIR}/module/third_part/protobuf-all-3.19.4.tar.gz)
+set(PROTOBUF_URL             ${CMAKE_SOURCE_DIR}/module/third_part/${PROTOBUF_NAME}.tar.gz)
 set(PROTOBUF_CONFIGURE       ${HIREDIS_SOURCE_PATH}/configure --prefix=/usr/local/)
 set(PROTOBUF_MAKE            make -j 8)
 set(PROTOBUF_INSTALL         make install)
